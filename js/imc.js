113a3e27e342bed41f513cdb2403e418133be2a0
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-04-16 23:07:13 -0300

    update

---
+++ js/imc.js
@@ -0,0 +1,5 @@
+const nome = document.getElementById('nome')
+const altura = document.getElementById('altura')
+const peso = document.getElementById('peso')
+const calcular = document.getElementById('calcular')
+const resultado = document.getElementById('resultado')
