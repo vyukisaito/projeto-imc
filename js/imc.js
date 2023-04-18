6388206f800d134a78a58f776c4cba4f8710dd55
Yuki Saito <124106467+vyukisaito@users.noreply.github.com>
2023-04-17 22:36:09 -0300

    update

--- js/imc.js
+++ js/imc.js
@@ -1,7 +1,36 @@
-const nome = document.getElementById('nome');;
-const altura = document.getElementById('altura')
-const peso = document.getElementById('peso');
 const calcular = document.getElementById('calcular');
-const resultado = document.getElementById('resultado');
 
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
 
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+        const IMC = (peso / (altura * altura)).toFixed(1)
+
+        let classificação = ''
+
+        if (IMC < 18.5) {
+            classificação = 'abaixo do peso.'
+        }else if (IMC < 25) {
+            classificação = 'com peso ideal. Parabéns'
+        }else if (IMC < 30) {
+            classificação = ' levemente acima do peso.'
+        }else if (IMC < 35) {
+            classificação = 'com obesidade grau I'
+        }else if (IMC < 40) {
+            classificação = 'com obesidade grau II'
+        }else {
+            classificação= 'com obesidade grau III. Cuidado!'
+        }
+
+            resultado.textContent = `${nome} seu IMC é ${IMC} e você está ${classificação}`
+
+    }else {
+        resultado.textContent = 'Favor colocar todos os valores!'
+    }
+}
+
+calcular.addEventListener('click', imc)
